e8d34dd31f7822d66c68786bf6c908b781efe84c
SchegolevOL <92328776+SchegolevOL@users.noreply.github.com>
2022-09-24 19:34:48 +0500

    Add project files.

---
+++ StudentReportCard.App/DataBase.cs
@@ -0,0 +1,26 @@
+﻿using DbConfigLib;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentReportCard.App
+{
+    public class DataBase : DbContext
+    {
+        protected DataBase()
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<StudetReport> TableStudetReports=>Set<StudetReport>();
+       
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(DbConfig.ImportFromJson("db.json").ToString());
+        }
+    }
+}
---
+++ StudentReportCard.App/StudetReport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentReportCard.App
+{
+    [Table ("tab_student_report")]
+    public class StudetReport
+    {
+        [Column ("id")]
+        [Required]
+        public int Id { get; set; }
+        [Column("first_name")]
+        [Required]
+        public string? FirstName { get; set; }
+        [Column("last_nane")]
+        [Required]
+        public string? LastName { get; set; }
+        [Column("patronymic")]
+        [Required]
+        public string? Patronymic { get; set; }
+        [Column("date_time")]
+        [Required]
+        public DateTime Date { get; set; }
+        [Column("assessment")]
+        [Required]
+        public int Assessment { get; set; }
+    }
+}
